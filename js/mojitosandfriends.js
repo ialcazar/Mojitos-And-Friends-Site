f49c75a26c4e3918467a50ba330917102b15ece8
ialcazar <israelalcazar@gmail.com>
2011-07-10 18:42:57 +0200

    Changed Index Page

--- js/mojitosandfriends.js
+++ js/mojitosandfriends.js
@@ -1,6 +1,30 @@
-$(document).ready(cargar);
+$(document).ready(start);
 
+function start(){
+	initEvents();
+	initEffects();
+	
+}
+function initEvents(){
+	$("#li-index").click(function(){
+		$("section.body section").hide();
+		$("#proximamente").slideDown();
+		
+		$("section.content header nav ul li").removeClass("selected");
+		$("#li-index").addClass("selected");
+	});
+	$("#li-about").click(function(){
+		$("section.body section").hide();
+		$("#de-que-va-esto").fadeIn();
+		
+		$("section.content header nav ul li").removeClass("selected");
+		$("#li-about").addClass("selected");
+	});
+}
 
+function initEffects(){
+	$("#proximamente").slideDown("slow");
+}
 function cargar() { 
 	if (GBrowserIsCompatible()) { 
 		var map2 = new GMap2(document.getElementById("map2")); 
