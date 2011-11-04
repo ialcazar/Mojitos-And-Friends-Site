75bf164606734b33ceb29db2d49c44d61f80ff75
ialcazar <israelalcazar@gmail.com>
2011-11-04 19:42:07 +0100

    Php Initial version

---
+++ mf-php/resources/js/script.js
@@ -0,0 +1,51 @@
+$(document).ready(start);
+
+function start(){
+	initEvents();
+	initEffects();
+	
+}
+function initEvents(){
+	$("#li-index").click(function(){
+		hideAllSections();
+		$("#proximamente").slideDown();
+		$("#attendees").fadeIn();
+		$("#map2").fadeIn();
+		$("#twitts").fadeIn();
+		
+		selectMenuOption("#li-index");
+	});
+	$("#li-about").click(function(){
+		hideAllSections();
+		$("#de-que-va-esto").fadeIn();
+		
+		selectMenuOption("#li-about");
+	});
+}
+
+function initEffects(){
+	$("#proximamente").slideDown("slow");
+}
+function selectMenuOption(sectionToSelect){
+	$("#container header nav ul li").removeClass("selected");
+	$(sectionToSelect).addClass("selected");
+	
+}
+
+function hideAllSections(){
+	$("#main section").hide();
+	
+}
+
+function loadGMaps() { 
+	      if (GBrowserIsCompatible()) {
+	        var map = new GMap2(document.getElementById("map2"));
+			var point = new GLatLng(40.42072, -3.699758);
+			map.addOverlay(new GMarker(point));
+	        map.setCenter(point, 17);
+			map.addControl(new GSmallMapControl());
+			map.addControl(new GMapTypeControl());
+	        map.openInfoWindow(map.getCenter(),document.createTextNode("El Tigre"));
+	      }
+}
+
